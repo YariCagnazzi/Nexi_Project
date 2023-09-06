4fc15096fb78ce18daad6fd3653adb966095145b
Yari <yari.cagnazzi@prometeia.com>
2023-09-06 15:23:08 +0200

    update file bash

--- Avvio_Rockman.bat
+++ Avvio_Rockman.bat
@@ -1,6 +1,18 @@
 @echo off
+
+:loop
 node "C:\Users\ycagnazzi\Desktop\richieste_query\Nexi_Project\main.js"
+set /p "choice=Sei sicuro di voler uscire? (y/n): "
+
+if /i "%choice%"=="y" (
+    goto :exit
+) else if /i "%choice%"=="n" (
+    goto :loop
+) else (
+    echo Scelta non valida. Per favore, inserisci "y" per uscire o "n" per continuare.
+    goto :loop
+)
+
+:exit
+echo Uscita confermata. Arrivederci!
 pause
-
-
-
