b63fecc5f3e3bce4ae83b702a177c1b74bbf4d79
Yari <yari.cagnazzi@prometeia.com>
2023-09-20 09:56:51 +0200

    add class environment

--- postman.js
+++ postman.js
@@ -50,5 +50,54 @@ class Collection {
 }
 
 
+class Environment {
+  constructor(collectionFile) {
+    const baseCollection = './environments/';
+    this.collectionFilePath = baseCollection + collectionFile;
+    this.environmentData = null;
+  }
 
-module.exports = { Collection };
+  validateFilePath() {
+    const osPlatform = process.platform;
+    const filePath = this.collectionFilePath;
+
+    // Converti il percorso relativo in un percorso assoluto
+    const absolutePath = path.resolve(filePath);
+
+    if (osPlatform === 'win32') {
+      // Validazione per Windows
+      if (!absolutePath.match(/^[a-zA-Z]:\\/)) {
+        throw new Error(`Invalid file path format on Windows. Use "C:\\path\\to\\collection.json"`);
+      }
+    } else if (osPlatform === 'darwin' || osPlatform === 'linux') {
+      // Validazione per macOS e Linux
+      if (!absolutePath.startsWith('/')) {
+        throw new Error(`Invalid file path format on ${osPlatform}. Use "/path/to/collection.json"`);
+      }
+    } else {
+      // Gestire altri sistemi operativi se necessario
+      throw new Error(`Unsupported operating system: ${osPlatform}`);
+    }
+  }
+
+  readAndParseEnvironment() {
+    this.validateFilePath();
+    try {
+      const environmentData = fs.readFileSync(this.collectionFilePath, 'utf8');
+      this.environmentData = JSON.parse(environmentData);
+    } catch (error) {
+      throw new Error(`Error reading or parsing the environment file: ${error.message}`);
+    }
+  }
+
+  getEnvironment() {
+    if (!this.environmentData) {
+      this.readAndParseEnvironment();
+    }
+    return this.environmentData;
+  }
+}
+
+
+
+module.exports = { Collection, Environment };
