ab6d896db31371089999e0bbbc3b1010a372ea51
Yari <yari.cagnazzi@prometeia.com>
2023-09-26 15:55:42 +0200

    update set InputVariables

--- collectionUtils.js
+++ collectionUtils.js
@@ -1,12 +1,15 @@
 const inquirer = require('inquirer');
 
-// Funzione per ottenere le variabili di input dall'oggetto collection  
+//costante per il prefisso delle variabili di input
+const INPUT_VARIABLE_PREFIX = "INPUT_";
+
+// Funzione per ottenere le variabili di input dall'oggetto collection
 function getInputVariables(collection) {
   const inputVariables = {};
 
   if (collection && collection.variable) {
     collection.variable.forEach((item) => {
-      if (item.key && item.key.startsWith("INPUT_")) {
+      if (item.key && item.key.startsWith(INPUT_VARIABLE_PREFIX)) {
         inputVariables[item.key] = item.value;
       }
     });
@@ -15,17 +18,21 @@ function getInputVariables(collection) {
   return inputVariables;
 }
 
-//funzione per setttare i valori di input aggiornando l'oggetto collection
+
+// Funzione per impostare i valori di input aggiornando l'oggetto collection
 function setInputVariables(collection, requiredKeys) {
   collection.variable = Object.keys(requiredKeys).map((key) => ({
     key,
     value: requiredKeys[key],
   }));
+
+  return collection;
 }
 
 
+
 // Funzione per ottenere i valori delle variabili di input dall'utente
-async function checkSelectedCollection(collection) {
+async function checkSelectedCollection(collection, environment) {
   const requiredKeys = getInputVariables(collection);
 
   const questions = Object.keys(requiredKeys).map((key) => ({
@@ -34,16 +41,21 @@ async function checkSelectedCollection(collection) {
     message: `Inserisci il valore per ${key}:`,
   }));
 
-  const answers = await inquirer.prompt(questions);
+  try {
+    const answers = await inquirer.prompt(questions);
 
-  // Aggiorna i valori delle variabili di input con le risposte dell'utente
-  Object.keys(answers).forEach((key) => {
-    requiredKeys[key] = answers[key];
-  });
+    // Aggiorna i valori delle variabili di input con le risposte dell'utente
+    Object.keys(answers).forEach((key) => {
+      requiredKeys[key] = answers[key];
+    });
 
-  setInputVariables(collection, requiredKeys);
-// console.info(collection);
-  return ;
+    const collectionUpdate = setInputVariables(collection, requiredKeys);
+
+   // Ritorna gli oggetti collectionUpdate e environment
+   return { collectionUpdate, environment };
+  } catch (error) {
+    console.error("Errore durante l'interazione con l'utente o l'esecuzione di Newman:", error);
+  }
 }
 
 
