b096dbaf9eb09e21c22dbc6e837f2dbb1dacf79c
Yari <yari.cagnazzi@prometeia.com>
2023-09-27 11:22:20 +0200

    add remove values

--- collectionUtils.js
+++ collectionUtils.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 
-//costante per il prefisso delle variabili di input
+// Costante per il prefisso delle variabili di input
 const INPUT_VARIABLE_PREFIX = "INPUT_";
 
 // Funzione per ottenere le variabili di input dall'oggetto collection
@@ -18,7 +18,6 @@ function getInputVariables(collection) {
   return inputVariables;
 }
 
-
 // Funzione per impostare i valori di input aggiornando l'oggetto collection
 function setInputVariables(collection, requiredKeys) {
   collection.variable = Object.keys(requiredKeys).map((key) => ({
@@ -28,8 +27,6 @@ function setInputVariables(collection, requiredKeys) {
 
   return collection;
 }
-
-
 
 // Funzione per ottenere i valori delle variabili di input dall'utente
 async function checkSelectedCollection(collection, environment) {
@@ -51,12 +48,43 @@ async function checkSelectedCollection(collection, environment) {
 
     const collectionUpdate = setInputVariables(collection, requiredKeys);
 
-   // Ritorna gli oggetti collectionUpdate e environment
-   return { collectionUpdate, environment };
+    // Ritorna gli oggetti collectionUpdate e environment
+    return { collectionUpdate, environment };
   } catch (error) {
     console.error("Errore durante l'interazione con l'utente o l'esecuzione di Newman:", error);
   }
 }
 
+// Procedura per rimuovere uno o più valori delle variabili di input dall'utente
+async function removeValues(collection) {
+  const inputVariables = getInputVariables(collection);
 
-module.exports = { checkSelectedCollection };
+  for (const key in inputVariables) {
+    const removeValue = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'removedValue',
+        message: `Inserisci il valore da rimuovere da '${key}':`,
+      },
+    ]);
+
+    const values = inputVariables[key].split(',').map(value => value.trim());
+    const removedValue = removeValue.removedValue.trim();
+
+    const updatedValues = values.filter(value => value !== removedValue);
+
+    inputVariables[key] = updatedValues.join(', ');
+  }
+
+  // Aggiorna i valori nell'oggetto originale collection
+  collection.variable.forEach((item) => {
+    if (item.key && item.key in inputVariables) {
+      item.value = inputVariables[item.key];
+    }
+  });
+
+  console.log(collection);
+  return ;
+}
+
+module.exports = { checkSelectedCollection, removeValues };
