7587ea3afa54479300d4fd6420d0d8ea2d36e9d0
Yari <yari.cagnazzi@prometeia.com>
2023-10-06 18:03:00 +0200

    update exit

--- Avvio_Rockman.bat
+++ Avvio_Rockman.bat
@@ -2,9 +2,13 @@
 
 :loop
 node "C:\Users\ycagnazzi\Desktop\richieste_query\Nexi_Project\main.js"
-set /p "choice=Sei sicuro di voler uscire? (y/n): "
+set /p "choice=Vuoi uscire? (y/n): "
 
 if /i "%choice%"=="y" (
+    call :conferma_uscita
+    if errorlevel 1 (
+        goto :loop
+    )
     goto :exit
 ) else if /i "%choice%"=="n" (
     goto :loop
@@ -12,6 +16,13 @@ if /i "%choice%"=="y" (
     echo Scelta non valida. Per favore, inserisci "y" per uscire o "n" per continuare.
     goto :loop
 )
+
+:conferma_uscita
+set /p "choice=Sei sicuro di voler uscire? (y/n): "
+if /i "%choice%"=="n" (
+    exit /b 1
+)
+exit /b 0
 
 :exit
 echo Uscita confermata. Arrivederci!
