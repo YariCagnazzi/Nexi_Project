87bf8ab4725fe1b2fef2ab4d993860c47a7787d3
Yari <yari.cagnazzi@prometeia.com>
2024-01-26 10:00:36 +0100

    add entry point

---
+++ rockman.js
@@ -0,0 +1,10 @@
+const RockmanModel = require('./rockmanModel');
+const RockmanView = require('./rockmanView');
+const RockmanController = require('./rockmanController');
+
+const model = new RockmanModel('collections', 'environments');
+const controller = new RockmanController(model);
+const view = new RockmanView(controller);
+
+// Start the application by running Rockman
+controller.runRockman();
