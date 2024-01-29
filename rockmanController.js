2be9e41a7c42be15b02d57a6e9a0fbabaeb5ea26
Yari <yari.cagnazzi@prometeia.com>
2024-01-29 10:18:50 +0100

    added controller documentation

--- rockmanController.js
+++ rockmanController.js
@@ -3,15 +3,18 @@ const RockmanView = require('./rockmanView');
 
 class RockmanController {
     constructor(model, view) {
+      // Initialize RockmanView instance with the current controller
       this.rockmanView = new RockmanView(this);
       this.model = model;
       this.view = view;
     }
   
     runRockman() {
+      // Get collection names from the model
       const collectionsNames = this.model.getCollectionsNames(); 
+      // Get environment names from the model
       const environmentsNames = this.model.getEnvironmentsNames();
-  
+      // Display the main menu
       this.rockmanView.showMainMenu(collectionsNames, environmentsNames);
     }
   }
