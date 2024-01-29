fd9d6fd21fce76511e9c68cac8f679f401e912f5
Yari <yari.cagnazzi@prometeia.com>
2024-01-29 10:35:00 +0100

    added model documentation

--- RockmanModel.js
+++ RockmanModel.js
@@ -1,16 +1,33 @@
 const { JSONCollectionReader } = require('./JSONCollectionReader');
 
-
+/**
+ * RockmanModel class represents a model that manages collections and environments.
+ */
 class RockmanModel {
+   /**
+   * Creates an instance of RockmanModel.
+   *
+   * @param {string} collections - The path to the collections folder.
+   * @param {string} environments - The path to the environments folder.
+   */
     constructor(collections, environments) {
       this.collectionsNames = new JSONCollectionReader(collections).readFolder();
       this.environmentsNames = new JSONCollectionReader(environments).readFolder();
     }
   
+     /**
+   * Gets the names of collections.
+   *
+   * @returns {Array<string>} - Names of collections.
+   */
     getCollectionsNames() {
       return this.collectionsNames;
     }
-  
+    /**
+   * Gets the names of environments.
+   *
+   * @returns {Array<string>} - Names of environments.
+   */
     getEnvironmentsNames() {
       return this.environmentsNames;
     }
