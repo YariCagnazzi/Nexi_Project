54a62ead979f8a447ab72c3254d3458bc4d5632f
Yari <yari.cagnazzi@prometeia.com>
2024-01-29 11:48:43 +0100

    added Rockman documentation

--- rockman.js
+++ rockman.js
@@ -2,6 +2,7 @@ const RockmanModel = require('./rockmanModel');
 const RockmanView = require('./rockmanView');
 const RockmanController = require('./rockmanController');
 
+// Create instances of the model, controller, and view
 const model = new RockmanModel('collections', 'environments');
 const controller = new RockmanController(model);
 const view = new RockmanView(controller);
